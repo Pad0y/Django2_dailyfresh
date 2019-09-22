021d086c3a60076709717a8ebccc5859b12c8a48
Pad0y <1148364963@qq.com>
2019-09-22 10:51:44 +0800

    购物车页面显示

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -12,37 +12,57 @@ class CartAddView(View):
     """购物车记录添加"""
 
     def post(self, request):
+        """购物车记录添加"""
         user = request.user
-        if user.is_authenticated:
+        if not user.is_authenticated():
+            # 用户为登录
             return JsonResponse({'res': 0, 'errmsg': '请先登录'})
 
+        # 接收数据
         sku_id = request.POST.get('sku_id')
         count = request.POST.get('count')
+
         # 数据校验
         if not all([sku_id, count]):
             return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
 
-        # 校验商品数目
+        # 检验添加的商品数量
         try:
             count = int(count)
         except Exception as e:
+            # 数目出错
             return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
 
         # 校验商品是否存在
         try:
             sku = GoodsSKU.objects.get(id=sku_id)
         except GoodsSKU.DoesNotExist:
+            # 商品不存在
             return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
 
+        # 业务处理 添加购物车记录
         conn = get_redis_connection('default')
         cart_key = 'cart_%d' % user.id
+        # 先尝试获取sku_id的值 -> hget cart_key 属性
+        # 如果sku_id在hash中不存在，hget返回None
         cart_count = conn.hget(cart_key, sku_id)
         if cart_count:
-            # 累加购物车商品数目
+            # 累加购物车中商品的数目
             count += int(cart_count)
 
-        # 校验商品库存
+        # 校验商品的库存
         if count > sku.stock:
             return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
+        # 设置hash中sku_id对应的值
+        # hset -> 如果sku_id已经存在，更新数据，如果sku_id不存在，添加数据
         conn.hset(cart_key, sku_id, count)
-        return JsonResponse({'res': 5, 'message': '添加成功'})
+
+        # 计算用户购物车中商品的条目数
+        # total_count = conn.hlen(cart_key)
+        total_count = 0
+        vals = conn.hvals(cart_key)
+        for val in vals:
+            total_count += int(val)
+
+        # 返回应答
+        return JsonResponse({'res': 5, 'total_count': total_count, 'message': '添加成功'})
