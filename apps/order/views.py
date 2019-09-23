b1e4268f253128e2bd669e99e1572ba86f32b99b
Pad0y <1148364963@qq.com>
2019-09-23 10:44:54 +0800

    order.virew

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import View
+from django_redis import get_redis_connection
+from user.models import Address
+from goods.models import GoodsSKU
+from utils.mixin import LoginRequiredMixin
 
-# Create your views here.
+
+# /order/place
+class OrderPlaceView(LoginRequiredMixin, View):
+    """提交订单页面显示"""
+
+    def post(self, request):
+        """提交订单页面显示"""
+        # 获取我们的登录用户
+        user = request.user
+        # 获取参数sku_ids
+        sku_ids = request.POST.getlist('sku_ids')
+
+        # 校验参数
+        if not sku_ids:
+            # cart page
+            return redirect(reverse('cart:show'))
+
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+
+        skus = []
+        # 保存商品的总家属和总价
+        total_count = 0
+        total_price = 0
+        # 便利sku_ids获取用户要购买的商品的信息
+        for sku_id in sku_ids:
+            # 根据商品的id获取商品的信息
+            sku = GoodsSKU.objects.get(id=sku_id)
+            # 获取用户要购买的商品的数量
+            count = conn.hget(cart_key, sku_id)
+            # 计算商品的小计
+            amount = sku.price * int(count)
+            # 动态给sku增加属性count,保存购买商品的数量
+            sku.count = count
+            # 动态给sku添加属性amount,保存购买商品的小计
+            sku.amount = amount
+            # 追加
+            skus.append(sku)
+            # 累加计算商品的总价和总剑术
+            total_count += int(count)
+            total_price += amount
+
+        # 云飞： 世纪开发的时候，属于一个子系统
+        transit_price = 10  # 写思
+
+        # 是付款
+        total_pay = total_price + transit_price
+
+        # 获取用户的首见地址
+        addrs = Address.objects.filter(user=user)
+
+        # 组织上下文
+        sku_ids = ','.join(sku_ids)  # [1, 25]->1, 25
+        context = {'skus': skus,
+                   'total_count': total_count,
+                   'total_price': total_price,
+                   'transit_price': transit_price,
+                   'total_pay': total_pay,
+                   'addrs': addrs,
+                   'sku_ids': sku_ids}
+
+        # 使用模板
+        return render(request, 'place_order.html', context)
