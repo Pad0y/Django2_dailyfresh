88ae69b20ca7d8ca0ce22d58d6943c9816201956
Pad0y <1148364963@qq.com>
2019-09-23 10:27:49 +0800

    cart.urls update

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from cart.views import CartAddView, CartInfoView
+from cart.views import *
 
 urlpatterns = [
     path('', CartInfoView.as_view(), name='show'),  # 购物车页面显示
-    path('/add', CartAddView.as_view(), name='add'),  # 购物车记录添加
+    path('add', CartAddView.as_view(), name='add'),  # 购物车记录添加
+    path('update', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
+    path('delete', CartDeleteView.as_view(), name='delete'),  # 购物车记录更新
 ]
